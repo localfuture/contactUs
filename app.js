e576782df080eb5898ee8b91e98fe2130a3b9474
Anand Alex <anandalex1993@gmail.com>
2019-08-27 11:27:44 +0530

    cors issue solved

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ app.set("view engine","ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// For CORS,Pgm Line no 12 to 29
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200' );
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 //mongoose.connect("mongodb://localhost:27017/contactUsDB",{useNewUrlParser: true});
 
 mongoose.connect("mongodb+srv://anand:unicornb1331@cluster0-0tquo.mongodb.net/contactUsDB?retryWrites=true&w=majority");
@@ -45,7 +66,6 @@ app.get("/displayMessages",(req,res) =>{
     });
 });
 
-var displayMessages = "http://localhost:3000/displayMessages";
 
 //////////////////////////// Search Phone Number /////////////////////
 app.get("/searchMobile/:name",(req,res)=>{
